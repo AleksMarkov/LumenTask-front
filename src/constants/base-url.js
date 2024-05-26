a5a497279bd837c478803bb80ef07634f58aae3a
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-05-26 00:18:06 -0400

    updated
    adjusted for deploy

--- src/constants/base-url.js
+++ src/constants/base-url.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "http://localhost:4000";
-// export const BASE_API_URL = "https://lumentask-server.onrender.com";
+// export const BASE_API_URL = "http://localhost:4000";
+export const BASE_API_URL = "https://lumentask-server.onrender.com";
